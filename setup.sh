aacae35d8f187e470f80ea2c1554012e2585aa82
Jan Wąsak <jan.wasak@hexagon.com>
2024-05-05 11:40:33 +0200

    nice terminality

--- setup.sh
+++ setup.sh
@@ -1,46 +1,51 @@
 #!/bin/bash
 
-echo "########################"
-echo "# aptitude packages"
-echo "# the fundaments of awesome"
-echo "########################"
+begin() {
+    echo "┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
+    echo "┃ $1"
+    echo "┃ -----"
+    echo "┃ $2"
+    echo "┗-------------------------------------------------------"
+}
 
-sudo apt-get update
-sudo apt-get upgrade
+finished() {
+    echo "┏-------------------------------------------------------"
+    echo "┃ Done with $1"
+    echo "┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
+}
+
+# Update package repositories and upgrade existing packages
+begin "aptitude packages" "the fundaments of awesome"
+echo "Updating package repositories"
+sudo apt-get update -y
+echo "Upgrading existing packages"
+sudo apt-get upgrade -y
+echo "Installing essential packages"
 sudo apt-get install -y zsh htop git curl tldr build-essential libssl-dev snapd
+finished "aptitude packages"
 
-echo "\n\n"
-
-echo "########################"
-echo "# zsh/antigen/starship"
-echo "# command line sweet sauce"
-echo "########################"
-
+# Configure zsh with antigen and starship prompt
+begin "zsh/antigen/starship" "command line sweet sauce"
+echo "Setting up zsh with antigen and starship"
 sudo usermod -s /usr/bin/zsh $(whoami)
 curl -L git.io/antigen > ~/antigen.zsh
 cp .zshrc ~/.zshrc
 cp .antigenrc ~/.antigenrc
 curl -sS https://starship.rs/install.sh | sh
+finished "zsh/antigen/starship"
 
-echo "\n\n"
-
-echo "########################"
-echo "# gogh/nerd-fonts"
-echo "# because what you're really after... is sway"
-echo "########################"
-
-bash -c  "$(wget -qO- https://git.io/vQgMr)" 
+# Install Gogh and Nerd Fonts
+begin "gogh/nerd-fonts" "because what you're really after... is sway"
+echo "Installing Gogh and Nerd Fonts"
+bash -c "$(wget -qO- https://git.io/vQgMr)"
 sudo apt install -y fonts-nerd-fonts dconf-editor
-# we've dumped our dconf for gnome terminal profiles, right?
-# dconf dump /org/gnome/terminal/legacy/profiles:/ > gogh-tokyo-night.dconf
-dconf load /org/gnome/terminal/legacy/profiles:/ < gogh-tokyo-night.dconf
+# Load GNOME Terminal profiles from file
+# dconf load /org/gnome/terminal/legacy/profiles:/ < gogh-tokyo-night.dconf
+finished "gogh/nerd-fonts"
 
-
-echo "########################"
-echo "# snaps"
-echo "# snap me up, bruh"
-echo "########################"
-
+# Install Snap packages
+begin "snaps" "snap me up, bruh"
+echo "Installing Snap packages"
 sudo snap install code --classic
 sudo snap install slack --classic
 sudo snap install postman --classic
@@ -48,28 +53,23 @@ sudo snap install gnome-boxes --classic
 sudo snap install 1password --classic
 sudo snap install gimp --classic
 sudo snap install spotify --classic
+finished "snaps"
 
-echo "\n\n"
-
-echo "########################"
-echo "# chrome"
-echo "# because of reasons"
-echo "########################"
-
+# Install Google Chrome
+begin "chrome" "because of reasons"
+echo "Installing Google Chrome"
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-sudo apt-get install -y ./google-chrome-stable_current_amd64.debecho 
+sudo apt-get install -y ./google-chrome-stable_current_amd64.debecho
+finished "chrome"
 
-echo "########################"
-echo "# docker"
-echo "# 'agua mala', the man said, 'you whore'"
-echo "########################"
-
+# Install Docker
+begin "docker" "'agua mala', the man said, 'you whore'"
+echo "Installing Docker"
 curl -fsSL https://get.docker.com -o get-docker.sh
 sudo sh get-docker.sh
+finished "docker"
 
-echo "\n\n"
-
-echo "########################"
-echo "EXECUTE THIS COMMAND AFTER REBOOT:"
+# Post-installation instructions
+begin "all done" "now it's time to..."
 echo "sudo groupadd docker; sudo usermod -aG docker $USER"
-echo "########################"
+finished "all done"
