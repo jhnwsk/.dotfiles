8068e22be531cb39549c89f454b2033325882030
Jan Wąsak <jan.wasak@hexagon.com>
2024-05-05 11:55:54 +0200

    install fira code for real this time

--- setup.sh
+++ setup.sh
@@ -16,17 +16,13 @@ finished() {
 
 # Update package repositories and upgrade existing packages
 begin "aptitude packages" "the fundaments of awesome"
-echo "Updating package repositories"
 sudo apt-get update -y
-echo "Upgrading existing packages"
 sudo apt-get upgrade -y
-echo "Installing essential packages"
 sudo apt-get install -y zsh htop git curl tldr build-essential libssl-dev snapd
 finished "aptitude packages"
 
 # Configure zsh with antigen and starship prompt
 begin "zsh/antigen/starship" "command line sweet sauce"
-echo "Setting up zsh with antigen and starship"
 sudo usermod -s /usr/bin/zsh $(whoami)
 curl -L git.io/antigen > ~/antigen.zsh
 cp .zshrc ~/.zshrc
@@ -36,16 +32,20 @@ finished "zsh/antigen/starship"
 
 # Install Gogh and Nerd Fonts
 begin "gogh/nerd-fonts" "because what you're really after... is sway"
-echo "Installing Gogh and Nerd Fonts"
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/FiraCode.zip
+unzip FiraCode.zip -d FiraCode
+sudo mkdir -p /usr/share/fonts/truetype/fira-code-nerd
+sudo cp FiraCode/* /usr/share/fonts/truetype/fira-code-nerd/
+sudo fc-cache -fv
+fc-list | grep "Fira Code"
 bash -c "$(wget -qO- https://git.io/vQgMr)"
-sudo apt install -y fonts-nerd-fonts dconf-editor
+sudo apt install -y dconf-editor
 # Load GNOME Terminal profiles from file
 # dconf load /org/gnome/terminal/legacy/profiles:/ < gogh-tokyo-night.dconf
 finished "gogh/nerd-fonts"
 
 # Install Snap packages
 begin "snaps" "snap me up, bruh"
-echo "Installing Snap packages"
 sudo snap install code --classic
 sudo snap install slack --classic
 sudo snap install postman --classic
@@ -57,14 +57,12 @@ finished "snaps"
 
 # Install Google Chrome
 begin "chrome" "because of reasons"
-echo "Installing Google Chrome"
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo apt-get install -y ./google-chrome-stable_current_amd64.debecho
 finished "chrome"
 
 # Install Docker
 begin "docker" "'agua mala', the man said, 'you whore'"
-echo "Installing Docker"
 curl -fsSL https://get.docker.com -o get-docker.sh
 sudo sh get-docker.sh
 finished "docker"
